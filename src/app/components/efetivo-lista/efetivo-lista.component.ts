622149c6809902a8f6b6b53233b328f6e1233923
flaviofss07 <flaviofss.07@gmail.com>
2019-10-20 19:54:39 -0300

    Correção no EfetivoServise fim

--- src/app/components/efetivo-lista/efetivo-lista.component.ts
+++ src/app/components/efetivo-lista/efetivo-lista.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { EfetivoService } from './app/efetivo.service';
+import { EfetivoService } from './../../efetivo.service';
 
 
 class Efetivo {
--- src/app/components/efetivo/efetivo.component.ts
+++ src/app/components/efetivo/efetivo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { EfetivoService } from 'efetivo.service';
+import { EfetivoService } from './../../efetivo.service';
 import { NgForm } from '@angular/forms';
 
 class Efetivo {
